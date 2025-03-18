ed2a536e32664e364fe2ea77c4daa61824b089bc
JavierSanApa <javi_sanchez10@outlook.com>
2025-03-18 19:17:12 +0100

    habilidades termindado y header scroll visual

--- main.js
+++ main.js
@@ -46,3 +46,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('.navbar');
+    
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
